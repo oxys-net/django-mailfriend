b13c2079cf267f6e5fd13123df8a94e13187123f
Mathieu Pillard <m.pillard@liberation.fr>
2010-05-14 17:56:18 +0200

    These fields shouldn't be required

--- mailfriend/forms.py
+++ mailfriend/forms.py
@@ -4,8 +4,8 @@ from mailfriend.models import MailedItem
 
 class MailedItemForm(forms.ModelForm):
 
-    user_email_as_from = forms.BooleanField(label="Use my e-mail address as from address")
-    send_to_user_also  = forms.BooleanField(label="Send myself a copy of this e-mail")
+    user_email_as_from = forms.BooleanField(label="Use my e-mail address as from address", required=False)
+    send_to_user_also = forms.BooleanField(label="Send myself a copy of this e-mail", required=False)
 
     class Meta:
         model = MailedItem
