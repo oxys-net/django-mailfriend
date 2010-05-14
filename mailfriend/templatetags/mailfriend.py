74e5e330449af1cbc4e6fb18b994580a3706b57f
Mathieu Pillard <m.pillard@liberation.fr>
2010-05-14 17:14:19 +0200

    Re-indent, correct one typo

--- mailfriend/templatetags/mailfriend.py
+++ mailfriend/templatetags/mailfriend.py
@@ -6,16 +6,16 @@ register = template.Library()
 
 @register.simple_tag
 def get_mail_to_friend_url(obj):
-  """
-  Given an object, returns the URL for its "mail to friend" URL. The object
-  must have a get_absolute_url method. If it does not, this template tag
-  will fail silently.
-  """
-  if hasattr(obj, 'get_absolute_url'):
-    try:
-      content_type = ContentType.objects.get_for_model(obj)
-      return reverse('mailfriend.views.mail_item_to_friend_form', args=[content_type.pk, obj.pk])
-    except:
-      return ''
-  else:
-    return ''
+    """
+    Given an object, returns the URL for its "mail to friend" URL. The object
+    must have a get_absolute_url method. If it does not, this template tag
+    will fail silently.
+    """
+    if hasattr(obj, 'get_absolute_url'):
+        try:
+          content_type = ContentType.objects.get_for_model(obj)
+          return reverse('mailfriend.views.mail_item_to_friend_form', args=[content_type.pk, obj.pk])
+        except:
+            return ''
+    else:
+        return ''
