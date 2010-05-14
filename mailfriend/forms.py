61708848e9a469ce580b641565d73a54c0d7cc60
Mathieu Pillard <m.pillard@liberation.fr>
2010-05-14 18:08:56 +0200

    Prevent spamming a bit by refusing to send the same message to the same user twice

--- mailfriend/forms.py
+++ mailfriend/forms.py
@@ -6,6 +6,17 @@ class MailedItemForm(forms.ModelForm):
 
     user_email_as_from = forms.BooleanField(label="Use my e-mail address as from address", required=False)
     send_to_user_also = forms.BooleanField(label="Send myself a copy of this e-mail", required=False)
+    
+    def clean(self):
+        dst = self.cleaned_data['mailed_to']
+        src = self.instance.mailed_by
+        content_type = self.instance.content_type
+        object_pk = self.instance.object_pk        
+        
+        if MailedItem.objects.filter(mailed_by=src, mailed_to=dst, object_pk=object_pk, content_type=content_type).count():
+            raise forms.ValidationError("You already sent a mail to this address about the same content!")
+
+        return self.cleaned_data
 
     class Meta:
         model = MailedItem
