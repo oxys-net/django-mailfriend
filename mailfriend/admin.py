f44c33cb0dcd385fa43ec838bc7e18dd45d7f0fe
Mathieu Pillard <m.pillard@liberation.fr>
2011-08-24 15:28:36 +0200

    Work around performance problems for ForeignKeys in django admin by using using raw_id_fields for users instead of a huge <select> with every user in the database...

--- mailfriend/admin.py
+++ mailfriend/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from mailfriend.models import MailedItem
+
+
+class MailedItemAdmin(admin.ModelAdmin):
+    raw_id_fields = ('mailed_by',)
   
-admin.site.register(MailedItem)
+admin.site.register(MailedItem, MailedItemAdmin)
