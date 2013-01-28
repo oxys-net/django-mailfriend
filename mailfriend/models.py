e8ce54ee41c4c91a690d73016bc095b4facc1c1d
David Charbonnier <d.charbonnier@oxys.net>
2013-01-28 07:47:37 +0100

    fix django 1.4 compatibility

--- mailfriend/models.py
+++ mailfriend/models.py
@@ -6,6 +6,9 @@ from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
 class MailedItem(models.Model):
     # Content-object field
     content_type   = models.ForeignKey(ContentType,
@@ -13,7 +16,7 @@ class MailedItem(models.Model):
             related_name="content_type_set_for_%(class)s")
     object_pk      = models.TextField(_('object ID'))
     content_object = generic.GenericForeignKey(ct_field="content_type", fk_field="object_pk")
-    mailed_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    mailed_by = models.ForeignKey(AUTH_USER_MODEL, blank=True, null=True)
     mailed_by_email = models.EmailField("Sender's E-mail")
     mailed_to = models.EmailField("Recipient's E-mail")
     user_email_as_from = models.BooleanField(default=False)
